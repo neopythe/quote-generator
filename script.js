69d71f2781f723ccf7231fd30ba3bedf7311a8e2
Evan Bay <neopythe@protonmail.ch>
2022-01-26 02:11:15 -0700

    implemented DOM manipulation

--- script.js
+++ script.js
@@ -1,14 +1,27 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+
 let apiQuotes = [];
 
 // show new quote
 function newQuote() {
   // pick a random quote from apiQuotes array
   const quote = apiQuotes[Math.trunc(Math.random() * apiQuotes.length)];
-  console.log(quote);
 
-  // pick a random quote from localQuotes array
-  // const quote = localQuotes[Math.trunc(Math.random() * localQuotes.length)];
-  // console.log(quote);
+  // check if author field is blank and replace with 'unknown'
+  !quote.author
+    ? (authorText.textContent = 'Unknown')
+    : (authorText.textContent = quote.author);
+
+  // check quote length to determine styling
+  quote.text.length > 120
+    ? quoteText.classList.add('long-quote')
+    : quoteText.classList.remove('long-quote');
+
+  quoteText.textContent = quote.text;
 }
 
 // get quotes from API
@@ -22,6 +35,16 @@ async function getQuotes() {
     // catch error here
   }
 }
+
+// tweet quote
+function tweetQuote() {
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+  window.open(twitterUrl, '_blank');
+}
+
+// event listeners
+newQuoteBtn.addEventListener('click', newQuote);
+twitterBtn.addEventListener('click', tweetQuote);
 
 // on load
 getQuotes();
